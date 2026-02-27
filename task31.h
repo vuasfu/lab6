2c84cbd5e3a4d5b003f98563e16702ac0a0a8552
vuasfu <vuasfu@outlook.com>
2026-02-27 10:41:37 +0500

    task31.h

--- task31.h
+++ task31.h
@@ -3,16 +3,31 @@
 
 #include <string>
 
+/// <summary>
+/// Базовый класс для представления человека.
+/// </summary>
 class Person {
   public:
+    /// <summary>Конструктор с параметрами.</summary>
+    /// <param name="name">Имя человека</param>
+    /// <param name="height">Рост в сантиметрах</param>
     Person(const std::string& name, int height);
+
+    /// <summary>Возвращает строковое представление человека.</summary>
+    /// <returns>Строка вида "Имя, рост: значение"</returns>
     std::string print() const;
+
+    /// <summary>Возвращает имя человека.</summary>
+    /// <returns>Имя</returns>
     std::string GetName() const { return name_; }
+
+    /// <summary>Возвращает рост человека.</summary>
+    /// <returns>Рост в сантиметрах</returns>
     int GetHeight() const { return height_; }
 
   private:
-    std::string name_;
-    int height_;
+    std::string name_;  ///< Имя человека
+    int height_;        ///< Рост в сантиметрах
 };
 
-#endif  // TASK1_H_
+#endif
